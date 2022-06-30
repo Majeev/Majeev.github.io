2192d19308a048d6f7c4d8c3c2b401b163a549c1
Jakub Majewski <majewski.j01@gmail.com>
2022-06-30 11:16:29 +0200

    fix: header items overlapping fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import css from './Header.module.css'
 import WelcomePhoto from '../../assets/programming.jpg'
+import hero from '../../assets/computers.png'
 
 const Header = () => {
     return (
@@ -7,21 +8,23 @@ const Header = () => {
             <header className={css.header_large}>
                 <div className={css.main_container}>
                     <div className={css.welcome}>
-                        <div className={css.welcome_description}>
-                            <h3>Hi! I am Jakub,</h3>
-                            <p> Front end Developer you are looking for!</p>
-                            <div className={css.contactMe}>Contact Me! </div>
+                        <div className={css.welcomeHero}>
+                            <img src={hero} className={css.hero}/>
+                            <div className={css.welcome_description}>
+                                <h3>Hi! I am Jakub,</h3>
+                                <p> Front end Developer you are looking for!</p>
+                                <div className={css.contactMe}>Contact Me!</div>
                             </div>
+                        </div>
                         <div className={css.welcome_traits}>
                             <span className={css.passion}> Passion </span>
                             <span className={css.desire}> Desire to learn </span>
                             <span className={css.patience}> Patience </span>
-                            <span className={css.creativty}> Creativity </span>
+                            <span className={css.creativity}> Creativity </span>
                             <span className={css.discipline}> Discipline </span>
                         </div>
                     </div>
                     <img src={WelcomePhoto} alt='programming' className={css.welcome_photo}/>
-
                 </div>
             </header>
         </div>
