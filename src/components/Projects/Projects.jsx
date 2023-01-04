8eb109157e342b9e325466c91e57e3ce88476a6b
Jakub_Majewski <majewski.j01@gmail.com>
2023-01-04 08:57:00 +0100

    fix: fix anchor for project from name to whole container

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -15,40 +15,46 @@ const Projects = () => {
                 <h3>Experience</h3>
             </header>
             <div className={css.projects__container}>
-                <div className={css.projects__card}>
-                    <a href="#"><h4>Portfolio</h4></a>
-                    <img src={portfolio} alt="Portfolio"/>
-                    <div className={css.projects__technologies}>
-                        <SiHtml5/>
-                        <SiCss3/>
-                        <SiJavascript/>
-                        <FaReact/>
-                        <a href= "https://github.com/Majeev/Majeev.github.io" target="_blank"><AiFillGithub /></a>
+                <a href="#"> 
+                    <div className={css.projects__card}>
+                        <h4>Portfolio</h4>
+                        <img src={portfolio} alt="Portfolio"/>
+                        <div className={css.projects__technologies}>
+                            <SiHtml5/>
+                            <SiCss3/>
+                            <SiJavascript/>
+                            <FaReact/>
+                            <a href= "https://github.com/Majeev/Majeev.github.io" target="_blank"><AiFillGithub /></a>
+                        </div>
                     </div>
-                </div>
-                <div className={css.projects__card}>
-                    <a href="https://majeev.github.io/React-To-Do-List/" target="_blank"><h4>Todo list</h4></a>
-                    <img src={todo} alt="todo list"/>
-                    <div className={css.projects__technologies}>
-                        <SiHtml5/>
-                        <SiCss3/>
-                        <SiJavascript/>
-                        <FaReact/>
-                        <a href= "https://github.com/Majeev/React-To-Do-List" target="_blank"><AiFillGithub /></a>
+                </a>
+                <a href="https://majeev.github.io/React-To-Do-List/" target="_blank">
+                    <div className={css.projects__card}>
+                        <h4>Todo list</h4>
+                        <img src={todo} alt="todo list"/>
+                        <div className={css.projects__technologies}>
+                            <SiHtml5/>
+                            <SiCss3/>
+                            <SiJavascript/>
+                            <FaReact/>
+                            <a href= "https://github.com/Majeev/React-To-Do-List" target="_blank"><AiFillGithub /></a>
+                        </div>
                     </div>
-                </div>
-                <div className={css.projects__card}>
-                    <a href="https://majeev.github.io/Pokedex/" target="_blank"><h4>Pokédex</h4></a>
-                    <img src={pokedex} alt="pokedex"/>
-                    <div className={css.projects__technologies}>
-                        <SiHtml5/>
-                        <SiCss3/>
-                        <SiJavascript/>
-                        <FaReact/>
-                        <AiFillApi/>
-                        <a href= "https://github.com/Majeev/Pokedex" target="_blank"><AiFillGithub /></a>
+                </a>
+                <a href="https://majeev.github.io/Pokedex/" target="_blank">
+                    <div className={css.projects__card}>
+                        <h4>Pokédex</h4>
+                        <img src={pokedex} alt="pokedex"/>
+                        <div className={css.projects__technologies}>
+                            <SiHtml5/>
+                            <SiCss3/>
+                            <SiJavascript/>
+                            <FaReact/>
+                            <AiFillApi/>
+                            <a href= "https://github.com/Majeev/Pokedex" target="_blank"><AiFillGithub /></a>
+                        </div>
                     </div>
-                </div>
+                </a>
             </div>
         </section>
     )
