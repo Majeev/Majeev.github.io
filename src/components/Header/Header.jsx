3b2f9d9792884f7c5897ba2e8b41181388ee2db1
Jakub_Majewski <majewski.j01@gmail.com>
2023-01-04 13:48:08 +0100

    feat: add scroll trigger for big screen

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ const Header = () => {
                     <span>Hello, I am Jakub.</span>
                     <h1><strong>Frontend Developer with passion that you are looking for!</strong></h1>
                     <p>Passion for creation, tenacity of work. As a developer, I will help you bring your ideas into being.</p>
+                    <span className={css.header__scroll}>SCROLL DOWN!</span>
             </header>
     )
 }
