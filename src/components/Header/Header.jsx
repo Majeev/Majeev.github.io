e8d5c31605fb745020dbcd60f78e8b3dfea2f9c9
Jakub_Majewski <majewski.j01@gmail.com>
2023-01-05 08:56:09 +0100

    feat: add scroll trigger feature while staying on top of the page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,29 @@
+import { useState } from "react"
 import css from "./Header.module.css"
 
+
 const Header = () => {
+
+        const [animation, setAnimation] = useState(true)
+
+        const disableAnimation = () => {
+                if (window.scrollY >= 1) {
+                    setAnimation(false)
+                } else {
+                    setAnimation(true)
+                }
+            }
+        
+            window.addEventListener("scroll", disableAnimation)
+        
+        
+        
     return (
             <header className={css.header__container}>
                     <span>Hello, I am Jakub.</span>
-                    <h1><strong>Frontend Developer with passion that you are looking for!</strong></h1>
-                    <p>Passion to create, tenacity of work. As a developer, I will help you bring your ideas into being.</p>
-                    <span className={css.header__scroll}>SCROLL DOWN!</span>
+                    <h1><strong>Frontend Developer with fervor that you are looking for!</strong></h1>
+                    <p>Passion to create, tenacity at work. As a developer, I will help you bring your ideas into being.</p>
+                    <span className={animation ? css.header__scroll : `${css.header__scroll} ${css.header__noAnim}` }>SCROLL DOWN!</span>
             </header>
     )
 }
