ff48b4b88ea7cdcf1d3cf5bd55a953ba2de2106d
Jakub_Majewski <majewski.j01@gmail.com>
2023-01-05 08:02:43 +0100

    fix: fix typo in paragraph

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
             <header className={css.header__container}>
                     <span>Hello, I am Jakub.</span>
                     <h1><strong>Frontend Developer with passion that you are looking for!</strong></h1>
-                    <p>Passion for creation, tenacity of work. As a developer, I will help you bring your ideas into being.</p>
+                    <p>Passion to create, tenacity of work. As a developer, I will help you bring your ideas into being.</p>
                     <span className={css.header__scroll}>SCROLL DOWN!</span>
             </header>
     )
