811e6c73ba8eb712b38c0af9ce8eb1794e015624
Jakub_Majewski <majewski.j01@gmail.com>
2023-01-11 11:18:13 +0100

    removed unused commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header/Header';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
 import Contact from './components/Contact/Contact';
-import Footer from './components/Footer/Footer';
 import Projects from './components/Projects/Projects';
 
 function App() {
