d087e81b5abda081d38bb19cc7b7bdddf813cfb9
Jakub_Majewski <majewski.j01@gmail.com>
2023-01-11 11:19:15 +0100

    removed unused folder

--- src/components/Footer/Footer.jsx
+++
@@ -1,8 +0,0 @@
-const Footer = () => {
-    return (
-        <footer>
-            <p>Copyright ©2022 by Jakub Majewski. All rights reserved.</p>
-        </footer>
-    );
-};
-export default Footer;
