220ff7b7d25aa1a12abc815d1e654597a2f9635b
Jakub_Majewski <majewski.j01@gmail.com>
2023-01-27 13:07:51 +0100

    skillks update

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -15,12 +15,24 @@ const Skills = () => {
             image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
         },
         {
+            name: 'Typescript',
+            image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+        },
+        {
             name: 'React',
             image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg',
         },
         {
-            name: 'Typescript',
-            image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+            name: 'Redux',
+            image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg',
+        },
+        {
+            name: 'Bootstrap',
+            image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain-wordmark.svg',
+        },
+        {
+            name: 'Tailwind',
+            image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg',
         },
         {
             name: 'Git',
@@ -38,6 +50,8 @@ const Skills = () => {
             name: 'Photoshop',
             image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-plain.svg',
         },
+        
+
     ];
 
     return (
